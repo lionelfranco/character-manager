5c4d16093a9b9a901eebc9e1e04ba1e78525c38e
ismail <ismail.elydi@gmail.com>
2019-10-01 11:40:59 +0200

    préparation du bouton View => récupere le tableau par ID

--- script.js
+++ script.js
@@ -1,39 +1,58 @@
-async function getCharacters() {
-    let result = await fetch("https://character-database.becode.xyz/characters")
-    const array = await result.json()
+//const axios = require("axios");
 
-    // data indispensable pour afficher les données du tableau
+window.onload = function () {
 
-    var str = '<div>'
+    const api = 'https://character-database.becode.xyz'
+    async function getCharacters() {
+        let result = await axios.get(`${api}/characters`)
+        const array = result.data
 
-    array.forEach(function (element) {
-        str += '<div>'
-        str += '<img src="data:image/jpeg;base64,' + element.image + '"/>'
-        str += '<h1>' + element.name + '</h1>';
-        str += '<p>' + element.shortDescription + '</p>';
-        str += '<div class="buttons">'
-        str += '<button class="viewHero">View</button>'
-        str += '<button class="editHero">Edit</button>'
-        str += '<button class="deleteHero">Delete</button>'
-        str += '</div>'
-        str += '</div>'
-    });
-    str += '</div>';
-    //str += '<button id="addHero">Add</button>'
-    document.getElementById("list").innerHTML = str;
+        // data indispensable pour afficher les données du tableau
+
+        var str = '<div>'
+
+        array.forEach(function (element) {
+            //str += '<div id="'+element.id+'">'
+            str += `<div>`
+
+            str += '<img src="data:image/jpeg;base64,' + element.image + '"/>'
+            str += '<h1>' + element.name + '</h1>';
+            str += '<p>' + element.shortDescription + '</p>';
+            str += `<div class="buttons"  name="${element.id}">`
+            str += `<button class="viewHero">View</button">`
+            str += `<button class="editHero">Edit</button">`
+            str += `<button class="deleteHero">Delete</button">`
+            str += '</div>'
+            str += '</div>'
+
+
+
+        });
+
+        str += '</div>';
+        //str += '<button id="addHero">Add</button>'
+        document.getElementById("list").innerHTML = str;
+        document.querySelectorAll("#list .viewHero").forEach((element) => {
+
+            element.addEventListener("click", async (e) => {
+                let id = e.target.parentElement.getAttribute("name")
+
+                let idHero = await axios.get(`https://character-database.becode.xyz/characters/${id}`)
+
+                console.log(await idHero.data)
+
+
+
+            })
+        })
+
+
+
+        //add even listner pour chaque click
+
+    }
+
+    getCharacters();
+
 
 }
-
-
-
-
-getCharacters();
-
-
-
-
-/* window.onload = function () {
-    document.getElementById("addHero").addEventListener("click", () => {
-        console.log("yo")
-    })
-} */
